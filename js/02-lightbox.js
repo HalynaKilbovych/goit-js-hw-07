bc39385de8f03ddc3be89487f4f8d5a3d8a31a2f
Halyna Kilbovych <vusochina.galina@gmail.com>
2022-11-22 23:38:39 +0200

    2 task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl  = document.querySelector(".gallery"); 
+
+const makeGalleryItemsMarkup = ({preview, original, description}) => { 
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `
+}
+
+const makeGalleryItems = galleryItems.map(makeGalleryItemsMarkup).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", makeGalleryItems);
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+  
